3e67be3bf38cc46fdeca075c8fa9097f1086f5ff
Dean <deanapesek99@gmail.com>
2019-10-01 19:14:31 -0600

    added the fetch for search. added the fetch for create person. still having issues with hitting API

---
+++ TestApplication/React/deploytest/src/APIControllers/CreatePersonController.js
@@ -0,0 +1,14 @@
+export default function personHelper(url, data) {
+    console.log(data)
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        cache: 'no-cache',
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(data)
+    })
+}
--- TestApplication/React/deploytest/src/Components/CreatePerson.js
+++ TestApplication/React/deploytest/src/Components/CreatePerson.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import personHelper from '../APIControllers/CreatePersonController'
 
 export default class createperson extends Component {
 
@@ -14,9 +14,9 @@ export default class createperson extends Component {
    }
 
    handleClick = () => {
-       fetch(this.state.searchString + this.state.firstname + this.state.lastname)
+        personHelper(this.state.searchString, [this.state.firstname, this.state.lastname])
        .then(response => response.json())
-       .then(data => this.setState({people: data}, () => {}))
+       .then(data => this.setState({people: data}, () => {console.log(data)}))
    }
     
     render() {
@@ -25,11 +25,11 @@ export default class createperson extends Component {
                 <div className="container">
                     <h3>Create A Person</h3>
                     <p>First Name:</p>
-                    <input type="text"/>
+                    <input type="text" className="form-control" onChange={({target: {value: firstname }})=> this.setState({firstname})} value={this.state.firstname.value}/>
                     <p>Last Name:</p>
-                    <input type="text" />
+                    <input type="text" className="form-control" onChange={({target: {value: lastname }})=> this.setState({lastname})} value={this.state.lastname.value}/>
                     <br/>
-                    <input type="submit" />
+                    <input type="submit" onClick={this.handleClick}/>
                 </div>
             </React.Fragment>
         )
