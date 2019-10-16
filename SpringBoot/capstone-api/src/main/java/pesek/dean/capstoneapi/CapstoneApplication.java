da51187f9cfeeac55bf80461c2c1895970d5b8b0
Dean <deanapesek99@gmail.com>
2019-10-15 21:07:23 -0600

    started adding logic to the rest controllers. added dependencies for postgresSQL and added the stuff for database information to the application.properties

--- SpringBoot/capstone-api/src/main/java/pesek/dean/capstoneapi/CapstoneApplication.java
+++ SpringBoot/capstone-api/src/main/java/pesek/dean/capstoneapi/CapstoneApplication.java
@@ -1,9 +1,13 @@
 package pesek.dean.capstoneapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class CapstoneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
--- SpringBoot/capstone-api/src/main/java/pesek/dean/capstoneapi/controllers/AppointmentRestController.java
+++ SpringBoot/capstone-api/src/main/java/pesek/dean/capstoneapi/controllers/AppointmentRestController.java
@@ -1,8 +1,36 @@
 package pesek.dean.capstoneapi.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import pesek.dean.capstoneapi.models.Appointment;
+import pesek.dean.capstoneapi.repositories.AppointmentJpaRepository;
 
 @RestController
 public class AppointmentRestController {
+	
+	AppointmentJpaRepository appRepo;
+	
+	@PostMapping(path="")
+	public void createAppointment(@RequestBody Appointment app) {
+		appRepo.saveAndFlush(app);
+	}
+	
+	@GetMapping(path="")
+	public List<Appointment> getAllAppointments() {
+		return appRepo.findAll();		
+	}
 
+	//add a get for all appointments for that day
+	
+	@DeleteMapping(path="/{appointmentId}")
+	public void deleteAppointmentById(@RequestParam("appointmentId")int appointmentId) {
+		appRepo.deleteById(appointmentId);
+	}
 }
